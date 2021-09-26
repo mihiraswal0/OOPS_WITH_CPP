36dc363cb0a2ab8725ca5b9473acdb392e89108d
mihiraswal0 <91006719+mihiraswal0@users.noreply.github.com>
2021-09-26 11:18:36 +0530

    Add files via upload

---
+++ PRACTICAL_2/TASK 3/task3a_code.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+char a; 
+cin>>a;  //for character type cin will only take the very first elemnt that will be enterd
+
+cout<<a;
+
+return 0;
+}
+
---
+++ PRACTICAL_2/TASK 3/task3b_code.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+char a[50];
+cin.get(a,50); //get line will take multiple charcter as input 
+cout<<a;
+return 0;
+}
+
