61a4619e6d0d7c183140c9debd1c81b57f44c75f
mihiraswal0 <91006719+mihiraswal0@users.noreply.github.com>
2021-10-09 17:54:59 +0530

    Add files via upload

---
+++ Practical_5/Task1/task1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a=5;
+short b=10;
+long c=12;
+float d=13.45f;
+double e=3232.332;
+char f='a';
+bool g=true;
+
+cout<<"Int: "<<a<<endl;
+cout<<"short: "<<b<<endl;
+cout<<"Long: "<<c<<endl;
+cout<<"Float: "<<d<<endl;
+cout<<"Double: "<<e<<endl;
+cout<<"char: "<<f<<endl;
+cout<<"boolean: "<<g<<endl;
+return 0;
+}
