fa989c58df2c4f0f51a184521550dd9736ba57ec
mihiraswal0 <91006719+mihiraswal0@users.noreply.github.com>
2021-10-09 16:37:04 +0530

    Add files via upload

---
+++ Practical_4/Task3/task3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+//using namespace std;
+
+
+namespace First{
+int add(int a,int b){
+return a+b;
+}
+}
+
+namespace Second{
+float add(float a,float b){
+return a+b;
+}
+}
+
+
+int main(){
+std::cout<<"Using First namespace ans:"<<add(2,2)<<std::endl;
+std::cout<<"Using Second namespace ans:"<<add(2.2,2.2)<<std::endl;
+}
