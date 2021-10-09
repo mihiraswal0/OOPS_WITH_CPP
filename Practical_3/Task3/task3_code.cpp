9b6fc81abe7ebff10daead2736e99e4c2e7eb833
mihiraswal0 <91006719+mihiraswal0@users.noreply.github.com>
2021-10-09 14:11:22 +0530

    Create task3_code.cpp

---
+++ Practical_3/Task3/task3_code.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+char a; 
+cin>>a;  //for character type cin will only take the very first elemnt that will be enterd
+
+cout<<a;
+
+return 0;
+}
